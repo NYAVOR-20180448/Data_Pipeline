027424e86e6f2fecfd071e1a9ec2053efa3833a9
NYAVOR-20180448 <66072855+NYAVOR-20180448@users.noreply.github.com>
2023-11-14 08:12:24 +0100

    Add files via upload

--- data_Pipeline.py
+++ data_Pipeline.py
@@ -1,7 +1,12 @@
 #from data_Pipeline import data_ingestion, data_pre_processing, spliting_data, tranin_and_testing
+import sys
+sys.path.append("/home/von-ray/Desktop/airflow/lib/pythonX.X/site-packages")
+import os
+os.environ['PYSPARK_PYTHON'] = '/home/von-ray/Desktop/airflow/bin/python'
+os.environ['PYSPARK_DRIVER_PYTHON'] = '/home/von-ray/Desktop/airflow/bin/python'
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date
 from pyspark.sql.functions import when
@@ -12,18 +17,17 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 
-# create Spark session
-spark = SparkSession.builder.appName("FlightsDataProcessing").getOrCreate()
 # data ingestion as pyspark dataframe
 def data_ingestion():
-
+    # create Spark session
+    spark = SparkSession.builder.appName("FlightsDataProcessing").getOrCreate()
     # read the .CVS file as pyspark dataframe
     flights_df= spark.read.csv("/home/von-ray/code spark/Sujet d'Exam/flights.csv",header=True, inferSchema=True)
     return flights_df
 
 # data pre-processing
 
-def data_pre_processing(flights_df):
+def data_pre_processing(flights_df, **kwargs):
     # Drop lines with missing values
     flights_df = flights_df.dropna()
 
@@ -40,13 +44,13 @@ def data_pre_processing(flights_df):
 
     return flights_df
 
-def spliting_data(flights_df):
+def spliting_data(flights_df, **kwargs):
     #data ramdonly splited into 80% training and 20% testing
     train_data, test_data = flights_df.randomSplit([0.8, 0.2], seed=42)
 
     return train_data, test_data
 
-def tranin_and_testing(train_data, test_data):
+def train_and_testing(train_data, test_data, **kwargs):
 
     # convert the col 'Carrier' that hold string data into numerical data under new col 'CarrierIndexTemp'
     indexer = StringIndexer(inputCol='Carrier', outputCol='CarrierIndexTmp')
@@ -77,12 +81,67 @@ def tranin_and_testing(train_data, test_data):
 
 
 
-
-with DAG(dag_id= "FlightsDataProcessing", start_date=datetime(2023,11,12), schedule_interval = "@hourly", catchup=False) as dag:
-    load_data= PythonOperator(task_id= "Data_Ingestion", python_callable=data_ingestion)
-    process_data= PythonOperator(task_id= "Data_Pre_Processing", python_callable=data_pre_processing)
-    split_data= PythonOperator(task_id= "Spliting_Data", python_callable=spliting_data)
-    train_test_data= PythonOperator(task_id= "Tranin_And_Testing", python_callable=tranin_and_testing)
+"""
+with DAG(dag_id= "FlightsDataProcessing", start_date=datetime(2023,11,14), schedule_interval = "@hourly", catchup=False) as dag:
+    load_data= PythonOperator(task_id= "Data_Ingestion", python_callable=data_ingestion, provide_context=True)
+    process_data= PythonOperator(task_id= "Data_Pre_Processing", python_callable=data_pre_processing, provide_context=True)
+    split_data= PythonOperator(task_id= "Spliting_Data", python_callable=spliting_data, provide_context=True)
+    train_test_data= PythonOperator(task_id= "Tranin_And_Testing", python_callable=tranin_and_testing, provide_context=True)
 
 load_data >> process_data >> split_data >> train_test_data
+"""
 
+# Définir les paramètres du DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 11, 14),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Initialiser le DAG
+dag = DAG(
+    'FlightsDataProcessing',
+    default_args=default_args,
+    description='Orchestration du pipeline de données avec PySpark et Apache Airflow',
+    schedule_interval= "@hourly",
+)
+
+# Tâche pour l'ingestion des données
+data_ingestion_task = PythonOperator(
+    task_id='data_ingestion',
+    python_callable=data_ingestion,
+    provide_context=True,
+    dag=dag,
+)
+
+
+# Tâche pour le prétraitement des données
+data_pre_processing_task = PythonOperator(
+    task_id='data_pre_processing',
+    python_callable=data_pre_processing,
+    provide_context=True,
+    dag=dag,
+)
+
+# Tâche pour la division des données
+spliting_data_task = PythonOperator(
+    task_id='spliting_data',
+    python_callable=spliting_data,
+    provide_context=True,
+    dag=dag,
+)
+
+# Tâche pour l'entraînement et l'évaluation du modèle
+train_and_test_task = PythonOperator(
+    task_id='train_and_test',
+    python_callable=train_and_testing,
+    provide_context=True,
+    dag=dag,
+)
+
+# Définir les dépendances entre les tâches
+data_ingestion_task >> data_pre_processing_task >> spliting_data_task >> train_and_test_task
